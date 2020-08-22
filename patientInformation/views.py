8e0fad0bdb414bffe0743bb06366dfdaafbd090f
Elijah <elijah.kane.1972@gmail.com>
2020-08-22 05:24:01 -0700

    Nevermind

--- patientInformation/views.py
+++ patientInformation/views.py
@@ -211,7 +211,6 @@ def delete_patient(request, slug):
     if request.user.is_authenticated:
         patient = get_object_or_404(PatientInformation, slug=slug)
         patient.delete()
-        patient.delete()
         return redirect('home')
     else:
         return redirect('login')
