255c077a4a26d955384de64a5f820a723a05fd9b
elijahl123 <elijah.kane.1972@gmail.com>
2020-09-08 09:41:37 -0700

    Email Button Revised 2

--- patientInformation/views.py
+++ patientInformation/views.py
@@ -170,9 +170,9 @@ def patient_page(request, slug):
         )
         email.content_subtype = 'html'
         if patient.patient_image:
-            email.attach_file(patient.patient_image.path)
+            email.attach_file(patient.patient_image.url)
         if patient.injury_image:
-            email.attach_file(patient.injury_image.path)
+            email.attach_file(patient.injury_image.url)
         email.send()
 
     context['form'] = form
